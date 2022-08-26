a7b537d4495a695d382b5fd8f8442d4a4f8b86f0
flutterbuddy1 <mayanksmind@gmail.com>
2022-08-27 00:54:17 +0530

    make it better

--- app.js
+++ app.js
@@ -5,50 +5,47 @@ const http = require("http");
 const { default: mongoose } = require("mongoose");
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
-const io = require("socket.io")(server);
+const Socket = require("socket.io");
+const io = Socket(server);
+
 var bodyParser = require('body-parser');
 
 
 const users = require('./routes/users.js');
 
 app.use(bodyParser.json());
-app.use("/user",users);
+app.use("/user", users);
 
 
-mongoose.connect(process.env.CONNECTION_URL).then(()=>{
-    server.listen(port,"0.0.0.0", () => {
+mongoose.connect(process.env.CONNECTION_URL).then(() => {
+    server.listen(port, "0.0.0.0", () => {
         console.log(`Server is running on port ${port}`);
-      });
-}).catch((error)=>{
+    });
+}).catch((error) => {
     console.log(error.message);
 });
 
 var clients = {};
+
 io.on("connection", socket => {
-    console.log(socket.id);
+    console.log("SOCKETS" ,socket.id);
     socket.emit("connected", socket.id);
     socket.on("join", (data) => {
-        console.log("User :",data,"Joined");
-        if(!clients[data]){
+        if(clients[data]){
+            delete clients[data];
             clients[data] = socket;
-            socket.emit("joined", data);
+        }else{
+            clients[data] = socket;
         }
     });
     socket.on("message", (data) => {
         console.log(data);
-        if(clients[data.id]){
-            clients[data.id].emit("message", data);
-        }
-    });
-
-    socket.on("disconnect",(data)=>{
-        delete clients[data];
-        console.log(clients);
+        if (clients[data.id]) clients[data.id].emit("message", data);
     });
 });
 
-app.get("/",(res,req)=>{
+app.get("/", (res, req) => {
     req.send({
-        "port":port
+        "port": port
     });
 });
